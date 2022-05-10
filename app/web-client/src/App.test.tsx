9de8d5e553fc9929982dd0fa832f6d4486c9c28b
Alessio Pragliola <alessio.pragliola@sighup.io>
2022-05-10 16:36:25 +0200

    [ rm-1899 ] feat: license fix

--- app/web-client/src/App.test.tsx
+++ app/web-client/src/App.test.tsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
--- app/web-client/src/react-app-env.d.ts
+++ app/web-client/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
+/**
+ * Copyright (c) 2017-present SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 /// <reference types="react-scripts" />
