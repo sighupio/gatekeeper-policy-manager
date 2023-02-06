8d0ceb4431cd67c4c10df389e28d6de16071a92a
Ramiro Algozino <ramiro@sighup.io>
2023-02-06 16:00:42 +0100

    chore: add license header

--- tests/e2e/tests/configurations.spec.ts
+++ tests/e2e/tests/configurations.spec.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2023 SIGHUP s.r.l All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
 import { test, expect } from '@playwright/test';
 
 test('page configurations snapshot', async ({ page }) => {
