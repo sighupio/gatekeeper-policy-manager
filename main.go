68ae571a673ce2ceeb96cc0a661e5704e790bb7f
Ramiro Algozino <ramiro@sighup.io>
2023-04-06 19:29:51 +0200

    chore: addlicense

--- main.go
+++ main.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2023 SIGHUP s.r.l All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 // The backend for Gatekeeper Policy Manager, a simple to use web-based UI for OPA Gatekeeper
