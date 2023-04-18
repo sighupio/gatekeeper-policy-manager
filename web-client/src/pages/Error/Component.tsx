fc0fb538e04320b54b58bb95d64cb1061eb5f502
Ramiro Algozino <ramiro@sighup.io>
2023-04-18 12:08:49 +0200

    fix(frontend): show error details fix #631 Frontend does not show the full error description

--- web-client/src/pages/Error/Component.tsx
+++ web-client/src/pages/Error/Component.tsx
@@ -12,11 +12,12 @@ import {
   EuiPage,
   EuiSpacer,
   EuiText,
+  EuiCallOut,
 } from "@elastic/eui";
-import {useContext, useEffect, useState} from "react";
-import {useLocation, useNavigate} from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { ErrorPageState } from "./types";
-import {ApplicationContext} from "../../AppContext";
+import { ApplicationContext } from "../../AppContext";
 
 function ErrorComponent() {
   const { state } = useLocation();
@@ -28,7 +29,7 @@ function ErrorComponent() {
     if (
       initialContext === undefined &&
       appContextData.context.currentK8sContext !== undefined
-    ){
+    ) {
       setInitialContext(appContextData.context.currentK8sContext);
     }
 
@@ -78,6 +79,9 @@ function ErrorComponent() {
             <h4>{(state as ErrorPageState)?.error?.action}</h4>
           </EuiText>
         </EuiFlexItem>
+        <EuiCallOut title="Error details" color="danger" iconType="error">
+          <p>{(state as ErrorPageState)?.error?.description}</p>
+        </EuiCallOut>
         <EuiFlexItem grow={false}>
           <EuiButton
             href={`/${(state as ErrorPageState)?.entity ?? ""}`}
