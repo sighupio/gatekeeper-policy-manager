1fa3de0afe57cc6c3162a95302dece0966c0060f
Ramiro Algozino <ramiro@sighup.io>
2023-10-24 12:42:57 +0200

    chore: linting

--- web-client/fork-ts-checker.config.js
+++ web-client/fork-ts-checker.config.js
@@ -7,6 +7,6 @@
 module.exports = {
   logger: {
     log: (message) => console.log(message),
-    error: (message) => console.error(message),
-  },
-};
+    error: (message) => console.error(message)
+  }
+}
