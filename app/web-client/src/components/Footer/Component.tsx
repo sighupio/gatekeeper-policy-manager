2ff7f43ded1945e2e2b3501952db5fd1d910f96a
Ramiro Algozino <ramiro@sighup.io>
2024-01-11 18:19:06 +0100

    Bump version: 1.0.9 → 1.0.10

--- app/web-client/src/components/Footer/Component.tsx
+++ app/web-client/src/components/Footer/Component.tsx
@@ -22,7 +22,7 @@ function FooterComponent() {
         <EuiFlexItem grow={false}>
           <EuiText size="s" className="dynamic">
             <p>
-              <strong>Gatekeeper Policy Manager v1.0.9</strong>
+              <strong>Gatekeeper Policy Manager v1.0.10</strong>
             </p>
           </EuiText>
           <EuiText size="s">
